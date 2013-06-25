6f45c4936b12d37ac377f19380a5ad5e39ec61f5
kirkegaard <hello@christiank.org>
2013-06-25 15:38:13 +0200

    add particles

---
+++ lab/javascript/particles/Utilities.js
@@ -0,0 +1,24 @@
+// Utilities
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          window.oRequestAnimationFrame      ||
+          window.msRequestAnimationFrame     ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
+
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getRandomFloat (min, max) {
+    return Math.random() * (max - min) + min;
+}
